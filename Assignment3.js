0c1d2d9deb712f2a09278f9916859c06421ebfde
Amit Ranjan <amitr@valuefy.com>
2020-02-06 16:43:09 +0530

    =Assignment

--- Assignment3.js
+++ Assignment3.js
@@ -1,4 +1,5 @@
 // Program for printing number from 1 to 50 after every 1 second
+/*
 var count = 0;
 var fun = function () {
   if (count < 50) {
@@ -13,8 +14,29 @@ var fun = function () {
 var id = setInterval(fun, 1000);
 
 
+*/
 
+var count = 1;
 
+var fun = function (value) {
+  setTimeout(function () {
+    fun3(value);
+  }, 1000);
+}
+
+var fun3 = function (value) {
+  console.log(value);
+  count += 1;
+  fun1();
+}
+
+var fun1 = function () {
+  if (count <= 50) {
+    fun(count);
+  }
+}
+
+fun1();
 
 
 
